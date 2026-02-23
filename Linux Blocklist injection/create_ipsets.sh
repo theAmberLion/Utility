6aae17a5f31d0ce3ab68b7d3acd52b8f6d7a478f
theAmberLion <90892959+theAmberLion@users.noreply.github.com>
2026-02-23 07:23:45 +0200

    Create create_ipsets.sh

---
+++ Linux Blocklist injection/create_ipsets.sh
@@ -0,0 +1,33 @@
+# https://www.spamhaus.org/drop/drop_v4.json
+
+ipset create blk_spamhaus hash:net family inet hashsize 4096 maxelem 20000
+
+
+# https://iplists.firehol.org/?ipset=dshield_30d
+
+ipset create blk_dshield_30d hash:net family inet hashsize 8192 maxelem 30000
+
+
+# https://lists.blocklist.de/lists/strongips.txt
+
+ipset create blk_strongips hash:net family inet hashsize 8192 maxelem 30000
+
+
+# https://blocklist.greensnow.co/greensnow.txt
+
+ipset create blk_greensnow hash:net family inet hashsize 8192 maxelem 30000
+
+
+# https://sblam.com/blacklist.txt
+
+ipset create blk_sblam hash:net family inet hashsize 8192 maxelem 30000
+
+
+# https://iplists.firehol.org/files/botscout_7d.ipset
+
+ipset create blk_botscout_7d hash:net family inet hashsize 8192 maxelem 30000
+
+
+# https://iplists.firehol.org/?ipset=php_dictionary_30d
+
+ipset create blk_phpdictionary hash:net family inet hashsize 8192 maxelem 30000
